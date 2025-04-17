f546ed053cb981e3e679ca5fb0dbda90be13b26d
yeonukchoi <chicyw0821@gmail.com>
2025-04-17 20:56:57 +0900

    css 추가

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <img src="/profile.jpg" alt="프로필 사진" />
-      <h1>최연욱</h1>
-      <p>📧 chio0821@naver.com</p>
-      <p><strong>나이:</strong> 19세</p>
+    <div className="container">
+      <img src="/profile.jpg" alt="프로필 사진" className="profile-img" />
+      <h1 className="name">최연욱</h1>
+      <p className="email">📧 chio0821@naver.com</p>
+      <p style={{color : "red"}}><strong>나이:</strong> 19세</p>
       <p><strong>학력:</strong> 삼일공업고등학교 사물인터넷과</p>
       <p><strong>사는 곳:</strong> 수원시 장안구 정자3동</p>
       <p><strong>꿈:</strong> 풀스택 개발자 & ios 앱개발자</p>
