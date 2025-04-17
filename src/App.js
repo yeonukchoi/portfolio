d5007bd6ae73aa6e671769d89cf0576f19ee1574
yeonukchoi <chicyw0821@gmail.com>
2025-04-17 20:39:22 +0900

    first react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img src="/profile.jpg" alt="프로필 사진" />
+      <h1>최연욱</h1>
+      <p>📧 chio0821@naver.com</p>
+      <p><strong>나이:</strong> 19세</p>
+      <p><strong>학력:</strong> 삼일공업고등학교 사물인터넷과</p>
+      <p><strong>사는 곳:</strong> 수원시 장안구 정자3동</p>
+      <p><strong>꿈:</strong> 풀스택 개발자 & ios 앱개발자</p>
+    </div>
+  );
+}
+
+export default App;
